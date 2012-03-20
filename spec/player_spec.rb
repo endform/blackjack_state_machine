88f886bf301ba6cee4df7f39d544c677770b2f24
endform <endform@gmail.com>
2012-03-19 20:13:40 -0700

    stub out player spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe Player do
+  it 'should start with 500 chips' do
+    player = Player.new
+    player.chips.should == 500
+  end
+
+  it 'should take max bet of 100' do
+  end
+
+  it 'should take only positive integer bets' do
+  end
+
+  it 'should always return :player_placed_bet' do
+  end
+
+  it 'should not error when given garbage' do
+  end
 end
