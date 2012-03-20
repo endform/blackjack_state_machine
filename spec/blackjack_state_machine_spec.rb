481a54e8b7b72702743c2516bd6ff6e8c3e71bbc
endform <endform@gmail.com>
2012-03-19 20:16:45 -0700

    stub out blackjack class epc

--- spec/blackjack_state_machine_spec.rb
+++ spec/blackjack_state_machine_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
 
 describe BlackjackStateMachine do
-  it 'should be a tautology' do
-    true.should == true
+  it 'should give wins to blackjacks' do
+    # player has blackjack, dealer 21
+    # dealer has blackjack, player 21
+  end
+
+  it 'should not win or push when player busts' do
+  end
+
+  it 'should give win to player when dealer busts and player does not bust' do
+  end
+
+  it 'should give win to player when player score > dealer score' do
+  end
+
+  it 'should push when player score equals dealer score and no blackjacks' do
+  end
+
+  it 'should push when both dealer and player have blackjack' do
   end
 end
